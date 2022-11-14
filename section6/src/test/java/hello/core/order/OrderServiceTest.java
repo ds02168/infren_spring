44fa1c7ff93356b691c25a7a1971fda055798f26
ds02168 <1995musso@gmail.com>
2022-11-14 15:04:29 +0900

    2022-11-14

--- section6/src/test/java/hello/core/order/OrderServiceTest.java
+++ section6/src/test/java/hello/core/order/OrderServiceTest.java
@@ -16,19 +16,19 @@ public class OrderServiceTest {
     OrderService orderService;
 
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         AppConfig appConfig = new AppConfig();
         memberService = appConfig.memberService();
         orderService = appConfig.orderService();
     }
 
     @Test
-    void creteOrder(){
+    void creteOrder() {
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId,"itemA",10000);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 }
