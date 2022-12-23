f50cbfcfb16aee9b2b6756e2f79e9d73fb6344bd
ds02168 <1995musso@gmail.com>
2022-12-23 10:54:24 +0900

    2022-12-23

---
+++ section9/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAccount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP)
+            return discountFixAccount;
+        else
+            return 0;
+    }
+}
